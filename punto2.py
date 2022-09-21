4adc4bb3a86422724155891ddfb16ad12ce1f9c2
Your Name <you@example.com>
2022-09-20 21:25:52 -0500

    primer momento

---
+++ punto2.py
@@ -0,0 +1,13 @@
+#2.Leer información de 10 frutas {nombre, color, precio} para preparar
+#un salpicón; el programa debe permitir mostrar las 10 frutas ingresadas
+#al mismo tiempo+(1)
+
+contador=0
+Salpicon =[]
+while (contador<10):
+    frutas={}
+    frutas ['nombre']=input("Ingrese el nombre de la fruta")
+    frutas ['color']=input("Ingrese el color de la fruta")
+    frutas ['precio']=int(input("Ingrese el nombre de la fruta"))
+contador+1
+print(Salpicon)
